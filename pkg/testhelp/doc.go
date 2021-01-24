14ef35b87177ccb609465ca3cd44782982cee0d0
Danielle Zephyr Malament <danielle.malament@gmail.com>
2021-01-24 04:13:27 -0500

    Add testhelp package doc file

---
+++ pkg/testhelp/doc.go
@@ -0,0 +1,6 @@
+/*
+Package testhelp contains functions and associated types intended to make testing various types of code easier.
+
+Currently, this includes code that should (or should not) panic.
+*/
+package testhelp
